902f2f55bdc6015b11235f8b9d9de14fbd97f921
Gabi Montes <gabicmontes@gmail.com>
2023-02-01 11:10:27 -0300

    feat: test teacher data

---
+++ database/factories/TeacherFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Teacher>
+ */
+class TeacherFactory extends Factory {
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $user = User::factory()->create();
+        return [
+            'name'    => $this->faker->name,
+            'user_id' => $user->id
+        ];
+    }
+}
