eee10284532f741292a1cebef79a3fc2a4503b53
Gabi Montes <gabicmontes@gmail.com>
2023-02-01 09:14:02 -0300

    feat: add softdeletes on models

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Student extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'name',
@@ -15,4 +16,14 @@ class Student extends Model
         'phone',
         'birth_date'
     ];
+
+    public function enrollments()
+    {
+        return $this->hasMany(Enrollment::class);
+    }
+
+    public function courses()
+    {
+        return $this->belongsToMany(Course::class, 'enrollments');
+    }
 }
